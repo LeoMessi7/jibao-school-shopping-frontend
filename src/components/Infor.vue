c12701e54709fcdbd5bc8461e6f047441281379c
86132 <1367027689@qq.com>
2021-12-09 00:25:23 +0800

    最新

--- src/components/Infor.vue
+++ src/components/Infor.vue
@@ -35,7 +35,7 @@
                       </el-col>
                       <el-col :xs="24" :md="12" :style="{height: '350px'}">
                         <div class="avatar-upload-preview">
-                          <img :src="options.img"  />
+                          <img :src="options.img"/>
                         </div>
                       </el-col>
                     </el-row>
@@ -68,20 +68,20 @@
                 </div>
                 <ul class="list-group list-group-striped">
                   <li class="list-group-item">
-                    <svg-icon icon-class="user" />用户名称
-                    <div class="pull-right">w</div>
+                    <span>用户名称:</span>
+                    <div class="pull-right">朱狗屎</div>
                   </li>
                   <li class="list-group-item">
-                    <svg-icon icon-class="phone" />手机号码
-                    <div class="pull-right">w</div>
+                    <span>手机号码:</span>
+                    <div class="pull-right">13033333333</div>
                   </li>
                   <li class="list-group-item">
-                    <svg-icon icon-class="email" />用户邮箱
-                    <div class="pull-right">w</div>
+                    <span>邮箱:</span>
+                    <div class="pull-right">1367878459@qq.com</div>
                   </li>
                   <li class="list-group-item">
-                    <svg-icon icon-class="date" />创建日期
-                    <div class="pull-right">h</div>
+                    <span>创建日期:</span>
+                    <div class="pull-right">1921.12.2</div>
                   </li>
                 </ul>
               </div>
@@ -92,9 +92,29 @@
               <div slot="header" class="clearfix">
                 <span>基本资料</span>
               </div>
-              <el-tabs >
+              <el-tabs v-model="activeName">
                 <el-tab-pane label="基本资料" name="userinfo">
-
+                  <el-form ref="form" :model="user" :rules="rules" label-width="80px">
+                    <el-form-item label="用户昵称" prop="nickName">
+                      <el-input v-model="user.nickName" />
+                    </el-form-item>
+                    <el-form-item label="手机号码" prop="phonenumber">
+                      <el-input v-model="user.phonenumber" maxlength="11" />
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                      <el-input v-model="user.email" maxlength="50" />
+                    </el-form-item>
+                    <el-form-item label="性别">
+                      <el-radio-group v-model="user.sex">
+                        <el-radio label="0">男</el-radio>
+                        <el-radio label="1">女</el-radio>
+                      </el-radio-group>
+                    </el-form-item>
+                    <el-form-item>
+                      <el-button type="primary" size="mini" @click="submit">保存</el-button>
+                      <el-button type="danger" size="mini" @click="close">关闭</el-button>
+                    </el-form-item>
+                  </el-form>
                 </el-tab-pane>
                 <el-tab-pane label="修改密码" name="resetPwd">
 
@@ -121,9 +141,18 @@ export default {
     sidebar,
 
   },
+
   data() {
     return {
       // 是否显示弹出层
+      activeName:'userinfo',
+        user: {
+          nickName: 'wo',
+          phonenumber: 13614511587,
+          email:'1367894568@qq.com',
+          sex:'nv'
+
+      },
       open: false,
       // 是否显示cropper
       visible: false,
@@ -136,7 +165,29 @@ export default {
         autoCropHeight: 200, // 默认生成截图框高度
         fixedBox: true // 固定截图框大小 不允许改变
       },
-      previews: {}
+      previews: {},
+      // 表单校验
+      rules: {
+        nickName: [
+          { required: true, message: "用户昵称不能为空", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "邮箱地址不能为空", trigger: "blur" },
+          {
+            type: "email",
+            message: "'请输入正确的邮箱地址",
+            trigger: ["blur", "change"]
+          }
+        ],
+        phonenumber: [
+          { required: true, message: "手机号码不能为空", trigger: "blur" },
+          {
+            pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
+            message: "请输入正确的手机号码",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
@@ -189,6 +240,16 @@ export default {
     // 实时预览
     realTime(data) {
       this.previews = data;
+    },
+    submit() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+            this.$message({message:"修改成功",type:'success'});
+        }
+      });
+    },
+    close() {
+      this.$router.push({ path: "/index" });
     }
   }
 }
@@ -234,5 +295,37 @@ export default {
   border-radius: 50%;
   box-shadow: 0 0 4px #ccc;
   overflow: hidden;
+}
+.list-group-item{list-style-type:none}
+.list-group-striped > .list-group-item {
+  border-left: 0;
+  border-right: 0;
+  border-radius: 0;
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.list-group {
+  padding-left: 0px;
+  list-style: none;
+}
+
+.list-group-item {
+  border-bottom: 1px solid #e7eaec;
+  border-top: 1px solid #e7eaec;
+  margin-bottom: -1px;
+  padding: 11px 0px;
+  font-size: 13px;
+}
+.clearfix:after {
+   visibility: hidden;
+   display: block;
+   font-size: 0;
+   content: " ";
+   clear: both;
+   height: 0;
+ }
+.pull-right {
+  float: right !important;
 }
 </style>
