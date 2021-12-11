dd0c984171971cf5821bc6d4137d49b92db86c22
LOTEAT <zengle@gmail.com>
2021-12-11 23:57:25 +0800

    ok

--- src/api/goods/goods.js
+++ src/api/goods/goods.js
@@ -36,13 +36,15 @@ export function uploadGoods(description, name,sub_category, price){
   const map = {description: description, name: name, sub_category: sub_category, price: price}
   return axios({
     method: 'POST',
-    url: 'http://localhost:8081/goods/upload',
+    url: 'goods/upload',
     headers: {
-      isToken: false
+      'Content-Type': 'application/json'
     },
     params: map
   })
 }
+
+
 
 export function withdrawGoods(gid){
   const map = {gid: gid}
