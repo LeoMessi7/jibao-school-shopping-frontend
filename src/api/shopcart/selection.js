f43972dd6cb2d411bd1b8d799389811df4f68e25
LOTEAT <zengle@gmail.com>
2021-12-13 01:29:22 +0800

    ok

--- src/api/shopcart/selection.js
+++ src/api/shopcart/selection.js
@@ -26,3 +26,19 @@ export function deleteShopcart(gid){
     params: map
   })
 }
+
+
+export function buyAll(gid_list, total){
+  let formData = new FormData()
+  formData.append("gid_list", gid_list)
+  formData.append("total", total)
+  return axios({
+    method: 'POST',
+    url: '/goods/buyAll',
+    headers: {
+      'Content-Type':'multipart/form-data'
+    },
+    data: formData
+  })
+
+}
